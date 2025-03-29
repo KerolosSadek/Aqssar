7f2533862a3ef398736611edac7b6463cffbca89
Kerolos Sadek <kokosadek55@gmail.com>
2025-03-29 16:34:20 +0200

    Implement Points in feedback

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 // Js File
+
+function handleAppDownload() {
+    // Track the click event
+    fbq('track', 'Downloads');
+
+    // Device detection
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (/android/i.test(userAgent)) {
+        window.location.href = 'https://play.google.com/store/apps/details?id=com.aqssar.aqssar';
+    } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        window.location.href = 'https://itunes.apple.com/sa/app/أقصر-القراءة-أسهل/id1178137933?mt=8';
+    } else {
+        // Default fallback
+        window.location.href = 'https://itunes.apple.com/sa/app/أقصر-القراءة-أسهل/id1178137933?mt=8';
+    }
+}
